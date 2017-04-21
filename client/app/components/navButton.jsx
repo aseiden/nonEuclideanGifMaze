eb6471f821ba237558aa219a9bfb8f4d9b5fd469
Alex Seiden <alex.seiden@gmail.com>
2017-04-21 16:16:32 -0700

    update currentLocation state based on nav clicks

--- client/app/components/navButton.jsx
+++ client/app/components/navButton.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 var NavButton = (props) => {
 
+  var handleClick = (direction) => {
+    props.handleNavClick(direction);
+  };
+
   return (
-    <button className={props.direction} type="button">{props.direction}</button>
+    <button 
+      className={props.direction} 
+      type="button"
+      onClick={() => handleClick(props.direction)}
+    >
+      {props.direction}
+    </button>
   );
 
 };
--- client/app/index.jsx
+++ client/app/index.jsx
@@ -5,23 +5,72 @@ import AwesomeComponent from './components/testComp.jsx';
 import NavButton from './components/navButton.jsx';
 
 class App extends React.Component {
-  // handleNorthClick
-  // handleEastClick
-  // handleWestClick
-  // handleSouthClick
-  // handle gif input
-  // handle caption input
+  constructor(props) {
+    super(props);
 
-  
+    this.state = {
+      currentLocation: [],
+    };
+
+    this.handleNavClick = this.handleNavClick.bind(this);
+  }
+
+  handleNavClick(direction) {
+    var newLoc = this.state.currentLocation;
+    if (this.checkForBacktrack(direction)) {
+      newLoc.pop();
+      this.setState({
+        currentLocation: newLoc,
+      });
+    } else {
+      newLoc.push(direction[0]);
+      this.setState({
+        currentLocation: newLoc,
+      });
+    }
+  }
+
+  checkForBacktrack(direction) {
+    if (direction[0] === 'N' && this.state.currentLocation[this.state.currentLocation.length - 1] === 'S') {
+      return true;
+    }
+
+    if (direction[0] === 'S' && this.state.currentLocation[this.state.currentLocation.length - 1] === 'N') {
+      return true;
+    }
+
+    if (direction[0] === 'E' && this.state.currentLocation[this.state.currentLocation.length - 1] === 'W') {
+      return true;
+    }
+
+    if (direction[0] === 'W' && this.state.currentLocation[this.state.currentLocation.length - 1] === 'E') {
+      return true;
+    }
+
+    return false;
+  }
   
   render () {
-    console.log(NavButton);
+
     return (
       <div>
-        <NavButton direction={'North'}/>
-        <NavButton direction={'East'}/>
-        <NavButton direction={'South'}/>
-        <NavButton direction={'West'}/>
+        <p>You are currently at {this.state.currentLocation}</p>
+        <NavButton 
+          direction={'North'}
+          handleNavClick={this.handleNavClick}
+        />
+        <NavButton 
+          direction={'East'}
+          handleNavClick={this.handleNavClick}
+        />
+        <NavButton 
+          direction={'South'}
+          handleNavClick={this.handleNavClick}
+        />
+        <NavButton 
+          direction={'West'}
+          handleNavClick={this.handleNavClick}
+        />
       </div>
     );
   }
